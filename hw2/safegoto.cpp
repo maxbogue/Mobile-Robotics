a3f0e349a3903e977a1fd43d2555df55630391e7
Max Bogue <max@maxbogue.com>
2012-04-03 01:51:22 -0400

    Now calculating local minima!

--- hw2/safegoto.cpp
+++ hw2/safegoto.cpp
@@ -10,6 +10,17 @@ const double MIN_DISTANCE = 0.02;
 const int SAMPLE_POINTS = 64;
 const int SAMPLE_SIZE = 8;
 
+class Point {
+public:
+    double x, y;
+    Point(double x, double y) : x(x), y(y) {}
+};
+
+ostream& operator<<(ostream &strm, const Point &p) {
+    strm << "(" << p.x << ", " << p.y << ")";
+    return strm;
+}
+
 double distance(double x1, double y1, double x2, double y2) {
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
@@ -18,7 +29,45 @@ double angle(double x1, double y1, double x2, double y2) {
     return atan2(y2 - y1, x2 - x1);
 }
 
+void sampleData(PlayerCc::RangerProxy &rp, double rd[]) {
+    int START = rp.GetRangeCount() / 2 - SAMPLE_POINTS * SAMPLE_SIZE / 2;
+    for (int i = 0; i < SAMPLE_POINTS; i++) {
+        rd[i] = 0;
+        int groupStart = START + i * SAMPLE_SIZE;
+        for (int j = groupStart; j < groupStart + SAMPLE_SIZE; j++) {
+            rd[i] += rp[j];
+        }
+        rd[i] = rd[i] / SAMPLE_SIZE;
+    }
+}
+
+Point laserToPoint(int i, double d) {
+    double a = (0.5 - (double)i / SAMPLE_POINTS) * PI;
+    return Point(cos(a) * d, sin(a) * d);
+}
+
+vector<Point> localMinima(double rd[]) {
+    pair<double,int> last(rd[0], 0);
+    bool newMin = true;
+    vector<Point> localMins;
+    for (int i = 1; i < SAMPLE_POINTS; i++) {
+        if (rd[i] > last.first) {
+            if (newMin) {
+                localMins.push_back(laserToPoint(last.second, last.first));
+                newMin = false;
+            } else {
+                last = pair<double,int>(rd[i], i);
+            }
+        } else {
+            last = pair<double,int>(rd[i], i);
+            newMin = true;
+        }
+    }
+    return localMins;
+}
+
 int main(int argc, char *argv[]) {
+    
     char* host = "localhost";
     int port = 9001;
     
@@ -46,6 +95,8 @@ int main(int argc, char *argv[]) {
     
     cout << "First waypoint: " << xt << ", " << yt << endl;
     
+    double rangerData[SAMPLE_POINTS];
+    
     for (;;) {
         
         robot.Read();
@@ -68,6 +119,13 @@ int main(int argc, char *argv[]) {
                 break;
             }
         }
+        
+        sampleData(rp, rangerData);
+        vector<Point> mins = localMinima(rangerData);
+        for (int i = 0; i < mins.size(); i++) {
+            cout << mins[i] << ", ";
+        }
+        cout << endl;
         
         double speed = 0;
         double at = angle(x, y, xt, yt);
