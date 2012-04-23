487f8d27f884843b2653c6e0bc823e6f5a4b4e98
Max Bogue <max@maxbogue.com>
2012-04-23 11:08:56 -0400

    Conform to my style.

--- hw4/mapper.cpp
+++ hw4/mapper.cpp
@@ -23,87 +23,83 @@ static int good;
 static double localMap[WIN_X][WIN_Y];
 
 static void display() {
-  
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluOrtho2D(	0, WIN_X, 0, WIN_Y );
 
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  
-  glBegin( GL_POINTS );
-  for( int x = 0; x < WIN_X; x++ ) {
-    for( int y = 0; y < WIN_Y; y++ ) {
-      glColor3f( 1-localMap[x][y], 1-localMap[x][y], 1-localMap[x][y] );
-      
-      glVertex2i(x, y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, WIN_X, 0, WIN_Y);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glBegin(GL_POINTS);
+    for (int x = 0; x < WIN_X; x++) {
+        for (int y = 0; y < WIN_Y; y++) {
+            glColor3f(1-localMap[x][y], 1-localMap[x][y], 1-localMap[x][y]);
+            glVertex2i(x, y);
+        }
     }
-  }
-  glEnd();
-  
-  // Flush the pipeline
-  glFlush();
+    glEnd();
 
-  // Swap the buffers now that we're done drawing.
-  glutSwapBuffers();
-  good = 1;
+    // Flush the pipeline
+    glFlush();
+
+    // Swap the buffers now that we're done drawing.
+    glutSwapBuffers();
+    good = 1;
 }
 
 void redisplay() {
-  if(good)
-    glutPostRedisplay();
+    if (good) glutPostRedisplay();
 }
 
 void* robotLoop(void* args) {
+    while (true) {
 
-  while(true) {
-    
-    redisplay();
-    
-    double turnrate, speed;
+        redisplay();
 
-    // read from the proxies
-    pRobot->Read();
+        double turnrate, speed;
 
-    // Here is where you do your robot stuff
-    // including presumably updating your map somehow
+        // read from the proxies
+        pRobot->Read();
 
-  }
+        // Here is where you do your robot stuff
+        // including presumably updating your map somehow
 
+    }
 }
 
 
 int main(int argc, char *argv[]) {
-  int port = 0;
-  char* host = "localhost";
+    int port = 0;
+    char* host = "localhost";
 
-  if (argc > 1) {
-    port = atoi( argv[2] );
-    host = argv[1];
-  }
+    if (argc > 1) {
+        port = atoi( argv[2] );
+        host = argv[1];
+    }
 
-  pRobot = new PlayerClient( host, port );
-  pPosition = new Position2dProxy( pRobot, 0 );
-  pSonar = new SonarProxy( pRobot, 0 );
+    pRobot = new PlayerClient(host, port);
+    pPosition = new Position2dProxy(pRobot, 0);
+    pSonar = new SonarProxy(pRobot, 0);
 
-  printf("player connected on port %d, proxies allocated\n", port);
-  pPosition->SetMotorEnable(1);
-  
-  //  pthread_mutex_init(&mut, NULL);
-  
-  pthread_t thread_id;
-  pthread_create(&thread_id, NULL, robotLoop, NULL);
-  
-  glutInit( &argc, argv );
-  glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE );
-  glutInitWindowPosition( 50, 50 );
-  glutInitWindowSize( WIN_X, WIN_Y );
-  glutCreateWindow( "Map" );
-  
-  // Callbacks
-  glutDisplayFunc( display );
+    printf("player connected on port %d, proxies allocated\n", port);
+    pPosition->SetMotorEnable(1);
 
-  glutMainLoop();
-   
-  return 0;
+    //  pthread_mutex_init(&mut, NULL);
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, robotLoop, NULL);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowPosition(50, 50);
+    glutInitWindowSize(WIN_X, WIN_Y);
+    glutCreateWindow("Map");
+
+    // Callbacks
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    return 0;
 }
