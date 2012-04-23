77b7bef93ed5be6629c776dc1628ef05d34b59fd
Max Bogue <max@maxbogue.com>
2012-04-23 19:41:18 -0400

    Homework 4 kinda works now!

--- hw4/mapper.cpp
+++ hw4/mapper.cpp
@@ -3,24 +3,41 @@
 //   Player/Stage example code.
 // Not guaranteed to be decent code style but seems to work OK.
 
-#include <GL/glut.h>
+#include <GLUT/glut.h>
 #include <libplayerc++/playerc++.h>
 #include <pthread.h>
+#include <cmath>
 
-using namespace PlayerCc;
+using namespace std;
 
-#define WIN_X 600
-#define WIN_Y 600
+#define WIN_X 800
+#define WIN_Y 800
 
-static PlayerClient *pRobot;
-static Position2dProxy *pPosition;
-static SonarProxy *pSonar;
+//const double D = sqrt(512);
+const int BLOCK = 4;
+
+const double PI = atan(1.0) * 4;
+const double SONAR[8][3] = {
+    {  7.5,  13.0,  90*PI/180 },
+    { 11.5,  11.5,  50*PI/180 },
+    { 15.0,   8.0,  30*PI/180 },
+    { 17.0,   2.5,  10*PI/180 },
+    { 17.0,  -2.5, -10*PI/180 },
+    { 15.0,  -8.0, -30*PI/180 },
+    { 11.5, -11.5, -50*PI/180 },
+    {  7.5, -13.0, -90*PI/180 }
+};
+
+static PlayerCc::PlayerClient *pRobot;
+static PlayerCc::Position2dProxy *pPosition;
+static PlayerCc::SonarProxy *pSonar;
 
 static int good;
 
 // values here should be between 0 and 1 to plot correctly.
 // 0 will plot as white and 1 will plot as black.
 static double localMap[WIN_X][WIN_Y];
+static double oddsMap[WIN_X][WIN_Y];
 
 static void display() {
 
@@ -52,7 +69,30 @@ void redisplay() {
     if (good) glutPostRedisplay();
 }
 
+double toOdds(double prob) {
+    return prob / (1 - prob);
+}
+
+double toProb(double odds) {
+    return odds / (odds + 1);
+}
+
+double piecewise(double x, double d) {
+    if (x < d - 5) {
+        return 0.5;
+    } else {
+        return 15;
+    }
+}
+
 void* robotLoop(void* args) {
+    PlayerCc::SonarProxy sp = *pSonar;
+    for (int i = 0; i < WIN_X; i++) {
+        for (int j = 0; j < WIN_Y; j++) {
+            localMap[i][j] = 0.5;
+            oddsMap[i][j] = 1;
+        }
+    }
     while (true) {
 
         redisplay();
@@ -62,15 +102,46 @@ void* robotLoop(void* args) {
         // read from the proxies
         pRobot->Read();
 
+        // Robot positions in cm, angle (yaw) in rads.
+        double rx = pPosition->GetXPos() * 100 + 400 * BLOCK;
+        double ry = pPosition->GetYPos() * 100 + 400 * BLOCK;
+        double ra = pPosition->GetYaw();
+
         // Here is where you do your robot stuff
         // including presumably updating your map somehow
+        for (int s = 0; s < 8; s++) {
+            double sd = sp[s] * 100;
+            if (sd < 500.0) {
+                // Sonar positions in cm and angle in rads.
+                double sx = rx + SONAR[s][0] * cos(ra) - SONAR[s][1] * sin(ra);
+                double sy = ry + SONAR[s][0] * sin(ra) + SONAR[s][1] * cos(ra);
+                double sa = ra + SONAR[s][2];
 
+                for (int d = 10; d < sd + 10; d += BLOCK) {
+                    for (double b = -PI / 12; b <= PI / 12; b += PI / 48) {
+                        double a = sa + b;
+                        int x = (int)(sx + d * cos(a)) / BLOCK;
+                        int y = (int)(sy + d * sin(a)) / BLOCK;
+                        cout << x << ", " << y << endl;
+                        cout << b << endl;
+                        cout << abs(b) << endl;
+                        cout << abs(b) * 6 / PI << endl;
+                        double u = pow(piecewise(d, sd), abs(b) / (PI / 6));
+                        cout << u << endl;
+                        double o = oddsMap[x][y] * u;
+                        cout << o << endl;
+                        oddsMap[x][y] = o;
+                        localMap[x][y] = toProb(o);
+                    }
+                }
+            }
+        }
     }
 }
 
 
 int main(int argc, char *argv[]) {
-    int port = 0;
+    int port = 6665;
     char* host = "localhost";
 
     if (argc > 1) {
@@ -78,9 +149,9 @@ int main(int argc, char *argv[]) {
         host = argv[1];
     }
 
-    pRobot = new PlayerClient(host, port);
-    pPosition = new Position2dProxy(pRobot, 0);
-    pSonar = new SonarProxy(pRobot, 0);
+    pRobot = new PlayerCc::PlayerClient(host, port);
+    pPosition = new PlayerCc::Position2dProxy(pRobot, 0);
+    pSonar = new PlayerCc::SonarProxy(pRobot, 0);
 
     printf("player connected on port %d, proxies allocated\n", port);
     pPosition->SetMotorEnable(1);
